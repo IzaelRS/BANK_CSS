124da4c38fb91a9eab4a6028c8075254e65561e5
Izael R Silva <87791471+IzaelRS@users.noreply.github.com>
2023-02-19 14:37:12 -0300

    Add files via upload

---
+++ nav_menu/script.js
@@ -0,0 +1,7 @@
+let sub_menu = document.querySelector('.sub_menu');
+
+let navegacao = document.querySelector('.navegacao');
+
+sub_menu.onclick = function () {
+     navegacao.classList.toggle('active');
+}
